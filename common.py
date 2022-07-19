0a578bdefe7ca607a6936bc412a7864fa46a72ba
Cristian Martinez <crcmartinezs@gmail.com>
2022-07-19 10:04:25 -0500

    Adicion file handler

--- common.py
+++ common.py
@@ -1,1 +1,12 @@
 import yaml
+
+_config = None
+
+def config():
+    global _config
+
+    if _config is None:
+        with open("config.yaml") as file:
+            _config = yaml.load(file, Loader=yaml.FullLoader)
+
+        return _config
--- xray.py
+++ xray.py
@@ -1,29 +1,22 @@
-import arcpy
-
 wrkspc = r'D:\OneDrive - Esri NOSA\Consultoria\LES\ELAS\FAC\Proyectos\INFRAESTRUCTURA DIFRA\Infraestructura DIFRA\DIFRA_GDB_SQL_15AGO2019.gdb'
 
-arcpy.env.workspace = wrkspc
-datasets =  arcpy.ListDatasets() + ['']
-print(datasets)
-for dataset in datasets:
-    for feature_class in arcpy.ListFeatureClasses(feature_dataset=dataset):
-        fields = [field.name for field in arcpy.ListFields(feature_class)]
-        print(feature_class, fields)
 
-for table in arcpy.ListTables():
-    fields = [field.name for field in arcpy.ListFields(feature_class)]
-    print(table, fields)
+import arcpy
 
 
-def detectRelationship():
-    rc_list = [c.name for c in arcpy.Describe(wrkspc).children if c.datatype == "RelationshipClass"]
-    rc_list
+class Checker:
+    def __init__(self, wrkspc):
+        arcpy.env.workspace = wrkspc
 
-    # List the names, origin, and destination of the relationship class
-    for rc in rc_list:
-        des_rc = arcpy.Describe(rc)
-        origin = des_rc.originClassNames
-        destination = des_rc.destinationClassNames
-        print (rc, origin, destination)
+    @property
+    def gdb_elements(self):
 
-detectRelationship()
+        elements = [{'Name': fc.name, 'Type': fc.dataType,
+                     'Children': [{'Name': c.name, 'Type': c.dataType} for c in fc.children]}
+                    for fc in arcpy.Describe(wrkspc).children]
+        return elements
+
+
+
+print(Checker(wrkspc).gdb_elements)
+
